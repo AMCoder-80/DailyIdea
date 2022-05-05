c1186878b1f05eb5896fa6e24005b62020ef6cd9
Alireza Mortezaei <mortezaei_alireza@yahoo.com>
2022-05-05 14:05:15 +0430

    some change

--- idea/views.py
+++ idea/views.py
@@ -162,8 +162,10 @@ def get_idea(request, pk):
 
 @csrf_exempt
 def save_req(request):
+    idea = models.Idea.objects.get(content__icontains=request.POST['content'])
     try:
-        models.Requester.objects.create(**request.POST)
+        req = models.Requester.objects.create(user=request.POST['user'], phone_number=request.POST['phone'], type=request.POST['type'])
+        idea.requester.add(req)
     except Exception as e:
         return HttpResponse(e)
 
